86e0dc4867c13dbba8271f3f024b7201236863cb
James Murrer <jamesmurrer@Jamess-MBP.local>
2025-02-07 16:46:55 -0500

    added doxygen (doxyfile) and example documentation

---
+++ docs/html/navtreeindex0.js
@@ -0,0 +1,11 @@
+var NAVTREEINDEX0 =
+{
+"annotated.html":[0,0],
+"classeggsizer_m_l.html":[0,0,0],
+"classes.html":[0,1],
+"eggsizerml_8h_source.html":[1,0,0],
+"files.html":[1,0],
+"hierarchy.html":[0,2],
+"index.html":[],
+"pages.html":[]
+};
