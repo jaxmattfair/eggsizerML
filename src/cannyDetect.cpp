ed19a1c11e1a76a24766a19c08cf2fac73987386
Jackson Fair <jaxsofair@gmail.com>
2025-02-18 19:12:43 -0500

    Added Gaussian Blur to Canny
    Added a Gaussian blur to canny edge detection. Improves performance on the Lake Trout samples significantly

--- src/cannyDetect.cpp
+++ src/cannyDetect.cpp
@@ -3,15 +3,15 @@
 // < ---------------------------------------- >
 // OpenCV Analysis
 void autoCanny(cv::Mat src, cv::Mat *dst, float sigma) {
+    cv::Mat blurSrc;
+    cv::GaussianBlur(src, blurSrc, cv::Size(7, 7), 0);
     std::vector<uchar> array;
-    // cv::Mat srcCopy;
-    // src->copyTo(srcCopy);
-    array.assign(src.data, src.data + src.total()*src.channels());
+    array.assign(blurSrc.data, blurSrc.data + blurSrc.total()*blurSrc.channels());
     std::nth_element(array.begin(), array.begin() + 1, array.end(), std::greater{});
     double v = array[1];
 
     double lower = std::max(0.0, (1.0 - sigma) * v);
     double upper = std::min(255.0, (1.0 + sigma) * v);
-    cv::Canny(src, *dst, lower, upper);
+    cv::Canny(blurSrc, *dst, lower, upper);
 }
 // < ---------------------------------------- >
