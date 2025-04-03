4046e005b04e777d337fa4aeaac3a3bf6ed7f6b4
Jackson Fair <jaxsofair@gmail.com>
2025-04-03 00:27:35 -0400

    Added Output and CLI Framework

--- include/eggsizerml.h
+++ include/eggsizerml.h
@@ -2,12 +2,12 @@
 #define EGGSIZERML_H
 
 // Qt Native Stuff
-#include <QMainWindow>
 #include <QFileDialog>
-#include <QStandardPaths>
 #include <QImageReader>
 #include <QImageWriter>
+#include <QMainWindow>
 #include <QMessageBox>
+#include <QStandardPaths>
 
 // OpenCV2 Inclusions
 #include <opencv2/core/core.hpp>
@@ -20,29 +20,38 @@ class eggsizerML;
 }
 QT_END_NAMESPACE
 
-class eggsizerML : public QMainWindow
-{
-    Q_OBJECT
+class eggsizerML : public QMainWindow {
+  Q_OBJECT
 
 public:
-    eggsizerML(QWidget *parent = nullptr);
-    ~eggsizerML();
+  eggsizerML(QWidget *parent = nullptr);
+  ~eggsizerML();
 
 private slots:
-    // < ---------------------------------------- >
-    // BASIC IMAGE UPLOAD/PROCESSING WITH OPENCV
-    void open();
-    void on_fileOpen_btn_clicked();
-    bool loadFile(const QString &fileName);
-    void showNextImage();
-    void showPreviousImage();
-    // < ---------------------------------------- >
+  // < ---------------------------------------- >
+  // BASIC IMAGE UPLOAD/PROCESSING WITH OPENCV
+  void open();
+  void on_fileOpen_btn_clicked();
+  bool loadFile(const QString &fileName);
+  void showNextImage();
+  void showPreviousImage();
+  void outputResult(const QString &filename,
+                    std::vector<std::string> &imageNames,
+                    const std::vector<int> &eggNumbers,
+                    const std::vector<double> &avgAreas,
+                    const std::vector<double> &otsusAreas,
+                    const std::vector<double> &blobAreas,
+                    const std::vector<double> &certainties, int outputFormat,
+                    int emitImage);
+  // < ---------------------------------------- >
+
+  void on_saveResults_btn_clicked();
 
 private:
-    Ui::eggsizerML *ui;
-    QStringList imageFiles;  // Store selected image paths
-    int currentImageIndex = 0; // Track which image is displayed
+  Ui::eggsizerML *ui;
+  QStringList imageFiles;    // Store selected image paths
+  int currentImageIndex = 0; // Track which image is displayed
 
-    void loadImageAtIndex(int index);
+  void loadImageAtIndex(int index);
 };
 #endif // EGGSIZERML_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include "../include/eggsizerml.h"
-
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    if (strcmp(argv[1], "--somefakecall") == 0) {
+      // run some CLI call here
+    }
+  } else {
     QApplication a(argc, argv);
     eggsizerML w;
     w.show();
     return a.exec();
+  }
 }
